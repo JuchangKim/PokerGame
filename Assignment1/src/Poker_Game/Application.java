bb82bb0962f7e107b771579eb9c90303cb30af3a
Billie501 <132241423+Billie501@users.noreply.github.com>
2024-07-19 14:06:04 +1200

    made a comment //testing testing in Application.java to see if it works

--- Assignment1/src/Poker_Game/Application.java
+++ Assignment1/src/Poker_Game/Application.java
@@ -12,6 +12,7 @@ public class Application {
     public static void main(String[] args) {
         PokerCLI pokerCLI = new PokerCLI();
         pokerCLI.start();
+        //testing testing
     }
 }
 
