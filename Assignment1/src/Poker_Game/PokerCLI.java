431208d2c8401ba5579bac1b28c8630ce04f0ca0
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-07-22 15:15:24 +0900

    Update PokerCLI.java

--- Assignment1/src/Poker_Game/PokerCLI.java
+++ Assignment1/src/Poker_Game/PokerCLI.java
@@ -8,6 +8,30 @@ package Poker_Game;
  *
  * @author user
  */
+import java.util.Scanner;
+
 public class PokerCLI {
-    
+    private PokerGame game;
+    private Scanner scanner;
+
+    public PokerCLI() {
+        game = new PokerGame();
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        System.out.println("Welcome to Poker!");
+        setupPlayers();
+        game.startGame();
+    }
+
+    private void setupPlayers() {
+        System.out.print("Enter the number of players: ");
+        int numPlayers = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= numPlayers; i++) {
+            System.out.print("Enter name for Player " + i + ": ");
+            String name = scanner.nextLine();
+            game.addPlayer(name, 1000); // Default chips for each player
+        }
+    }
 }
