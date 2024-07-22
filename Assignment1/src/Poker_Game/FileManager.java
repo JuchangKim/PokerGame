09368ab9483102454847431d2cf21b21d327895e
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-07-22 15:14:00 +0900

    Update FileManager.java

--- Assignment1/src/Poker_Game/FileManager.java
+++ Assignment1/src/Poker_Game/FileManager.java
@@ -8,6 +8,24 @@ package Poker_Game;
  *
  * @author user
  */
+import java.io.*;
+import java.util.List;
+
 public class FileManager {
-    
+    public static void saveGameState(GameState gameState, String filePath) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(gameState);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static GameState loadGameState(String filePath) {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))) {
+            return (GameState) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
