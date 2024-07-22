255b4142d89485d9b9000b80c7436f4972bbd833
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-07-22 15:14:48 +0900

    Update Hand.java

--- Assignment1/src/Poker_Game/Hand.java
+++ Assignment1/src/Poker_Game/Hand.java
@@ -8,6 +8,35 @@ package Poker_Game;
  *
  * @author user
  */
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.List;
+
 public class Hand {
+    private List<Card> cards;
+
+    public Hand(List<Card> cards) {
+        this.cards = new ArrayList<>(cards);
+        this.cards.sort(Comparator.comparingInt(Card::getValue));
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void addCard(Card card)
+    {
+        cards.add(card);
+        cards.sort(Comparator.comparingInt(Card::getValue));
+    }
+    @Override
+    public String toString() {
+        return cards.toString();
+    }
+
+    public void clear() {
     
+        this.cards = null;
+    }
 }
