58bb84529253c8accb87e6a63a74aa1c514206f5
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-07-22 15:15:46 +0900

    Update PokerGame.java

--- Assignment1/src/Poker_Game/PokerGame.java
+++ Assignment1/src/Poker_Game/PokerGame.java
@@ -8,6 +8,61 @@ package Poker_Game;
  *
  * @author user
  */
+import java.util.ArrayList;
+import java.util.List;
+
 public class PokerGame {
-    
+    private List<Player> players;
+    private Deck deck;
+    private BettingSystem bettingSystem;
+    private List<Card> communityCards;
+
+    public PokerGame() {
+        players = new ArrayList<>();
+        deck = new Deck();
+        bettingSystem = new BettingSystem();
+        communityCards = new ArrayList<>();
+    }
+
+    public void addPlayer(String name, int chips) {
+        players.add(new Player(name, chips));
+    }
+
+    public void startGame() {
+        // Implement the game logic here
+        deck.reset();
+        communityCards.clear();
+        for(Player player : players)
+        {
+            player.getHand().clear();
+            
+        }
+        
+        // Release Initial cards
+        for(int i = 0; i < 2; i++)
+        {
+            for(Player player : players)
+            {
+                player.addCardToHand(deck.getCard());
+            }
+        }
+        
+        // Game flow
+        
+        
+    }
+/*
+    public void dealCards() {
+        for (Player player : players) {
+            for (int i = 0; i < 5; i++) {
+                player.addCardToHand(deck.dealCard());
+            }
+        }
+    }
+*/
+    public void showHands() {
+        for (Player player : players) {
+            System.out.println(player);
+        }
+    }
 }
