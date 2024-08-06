8521cb3c76b6858b0864f3d91232108d19243745
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-08-06 19:05:58 +1200

    Update PokerCLI.java

--- Assignment1/src/Poker_Game/PokerCLI.java
+++ Assignment1/src/Poker_Game/PokerCLI.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Poker_Game;
 
-/**
- *
- * @author user
- */
 import java.util.Scanner;
 
 public class PokerCLI {
@@ -19,19 +11,18 @@ public class PokerCLI {
         scanner = new Scanner(System.in);
     }
 
-    public void start() {
+    public void start() throws InterruptedException {
         System.out.println("Welcome to Poker!");
         setupPlayers();
         game.startGame();
     }
 
-    private void setupPlayers() {
-        System.out.print("Enter the number of players: ");
-        int numPlayers = Integer.parseInt(scanner.nextLine());
-        for (int i = 1; i <= numPlayers; i++) {
-            System.out.print("Enter name for Player " + i + ": ");
-            String name = scanner.nextLine();
-            game.addPlayer(name, 1000); // Default chips for each player
+    private void setupPlayers() throws InterruptedException {
+        
+        game.addPlayer("User", 1000); // Add user with 1000 chips
+        for (int i = 1; i < 5; i++) {
+            Thread.sleep(1000);
+            game.addPlayer("Computer" + " " + i, 1000); // Add computer players
         }
     }
 }
