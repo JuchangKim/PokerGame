c464c0b6039a70fc68fe64ad9e3ecfed5c87d2cc
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-08-06 20:10:49 +1200

    Add files via upload
    Those are the each round behaviour and GameStateAction is interface.

---
+++ Assignment1/src/Poker_Game/DealTurnState.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Poker_Game;
+
+/**
+ *
+ * @author user
+ */
+public class DealTurnState implements GameStateAction {
+    @Override
+    public void play(PokerGame game) throws InterruptedException {
+        game.getGameState().getCommunityCards().add(game.getDeck().getCard());
+        
+        Thread.sleep(1000); // Introduce delay
+    }
+}
---
+++ Assignment1/src/Poker_Game/GameStateAction.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Poker_Game;
+
+/**
+ *
+ * @author user
+ */
+public interface GameStateAction {
+    void play(PokerGame game) throws InterruptedException;
+}
