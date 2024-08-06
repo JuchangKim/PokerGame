6c73ed72cd422532ba04d99e0bfff917fd245371
JuchangKim <132308875+JuchangKim@users.noreply.github.com>
2024-08-06 20:11:35 +1200

    Update GameState.java

--- Assignment1/src/Poker_Game/GameState.java
+++ Assignment1/src/Poker_Game/GameState.java
@@ -13,10 +13,16 @@ import java.util.List;
 
 public class GameState implements Serializable {
     private List<Player> players;
+    private List<Card> communityCards;
+    private int pot;
+    private int currentBet;
     private int currentPlayerIndex;
 
-    public GameState(List<Player> players, int currentPlayerIndex) {
+    public GameState(List<Player> players, List<Card> communityCards, int pot, int currentBet, int currentPlayerIndex) {
         this.players = players;
+        this.communityCards = communityCards;
+        this.pot = pot;
+        this.currentBet = currentBet;
         this.currentPlayerIndex = currentPlayerIndex;
     }
 
@@ -24,7 +30,31 @@ public class GameState implements Serializable {
         return players;
     }
 
+    public List<Card> getCommunityCards() {
+        return communityCards;
+    }
+
+    public int getPot() {
+        return pot;
+    }
+
+    public void setPot(int pot) {
+        this.pot = pot;
+    }
+
+    public int getCurrentBet() {
+        return currentBet;
+    }
+
+    public void setCurrentBet(int currentBet) {
+        this.currentBet = currentBet;
+    }
+
     public int getCurrentPlayerIndex() {
         return currentPlayerIndex;
+    }
+
+    public void setCurrentPlayerIndex(int currentPlayerIndex) {
+        this.currentPlayerIndex = currentPlayerIndex;
     }
 }
